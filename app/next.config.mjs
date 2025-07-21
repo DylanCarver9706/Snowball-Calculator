3bde27856f5ab93721abe0836c6b216e2bc33a36
Dylan Carver <dylancarver14@gmail.com>
2025-07-20 19:34:39 -0500

    Update next config vars so Amplify detects them

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  },
+};
 
 export default nextConfig;
