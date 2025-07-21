bef1e74a0f88ac1921d8cde4a49d6bc1725e298c
Dylan Carver <dylancarver14@gmail.com>
2025-07-20 20:16:52 -0500

    Remove env vars from next.config.js to increase security

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
